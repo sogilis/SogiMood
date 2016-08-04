8db06d6ecb51aa6434bd718bb59f7d561b39bd26
Alexandre Dumont <dmt.alexandre@gmail.com>
2016-08-04 21:44:51 +0200

    change api with json field names

--- api.go
+++ api.go
@@ -1,7 +1,5 @@
 package main
 
-import "time"
-
 // Various values describing mood types.
 const (
 	Unknown string = "unknown"
@@ -13,19 +11,19 @@ const (
 
 // Project is the view of a project as exposed through the API.
 type Project struct {
-	ID          uint
-	Name        string
-	Description string
-	startedAt   *time.Time    // start date
-	dueAt       *time.Time    // deadline as set in the deal
-	finishedAt  *time.Time    // real end date
-	MoodsByWeek map[uint]Mood // moods by week number
+	ID          uint          `json:"id"`
+	Name        string        `json:"name"`
+	Description string        `json:"description"`
+	StartedAt   int64         `json:"started_at"`    // start date
+	DueAt       int64         `json:"due_at"`        // deadline as set in the deal
+	FinishedAt  int64         `json:"finished_at"`   // real end date
+	MoodsByWeek map[uint]Mood `json:"moods_by_week"` // moods by week number
 }
 
 // Mood is the view of a mood as exposed through the API.
 type Mood struct {
-	Customer string // is the customer happy?
-	Team     string // is the team happy?
-	Money    string // is the project worth it?
-	Details  string // side notes
+	Customer string `json:"customer"` // is the customer happy?
+	Team     string `json:"team"`     // is the team happy?
+	Money    string `json:"money"`    // is the project worth it?
+	Details  string `json:"details"`  // side notes
 }
--- db.go
+++ db.go
@@ -6,6 +6,6 @@ func keyForProject(projectID string) string {
 	return fmt.Sprintf("project:%s", projectID)
 }
 
-func keyForMood(projectID string, weekNo int) string {
-	return fmt.Sprintf("project:%s:week:%d", projectID, weekNo)
+func keyForMood(projectID, weekNo string) string {
+	return fmt.Sprintf("project:%s:week:%s", projectID, weekNo)
 }
