6c65c95acde7ed5b3ecbefaa8c1b988838c9569f
Alexandre Dumont <alexandre@sogilis.com>
2016-08-18 09:22:25 +0200

    add a basic authentication middleware (and refactor a bit)

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/garyburd/redigo/redis"
-	"github.com/gorilla/mux"
 )
 
 var (
@@ -28,37 +27,7 @@ func newPool(redisURL string) *redis.Pool {
 	}
 }
 
-func appHandler() *mux.Router {
-	router := mux.NewRouter()
-	router.HandleFunc("/", iAmRoot)
-	router.HandleFunc("/projects", listProjects).Methods("GET")
-	router.HandleFunc("/project", removeProject).Methods("DELETE").Queries("id", "{id}")
-	router.HandleFunc("/project", newProject).Methods("POST").Headers("Content-Type", "application/json")
-	router.HandleFunc("/mood", setMood).Methods("POST").Headers("Content-Type", "application/json").Queries("id", "{id}", "weekNo", "{weekNo:[0-9]+}")
-	return router
-}
-
-// CORSMiddleware is a mux.Router wrapper that enables CORS on that router.
-type CORSMiddleware struct {
-	router *mux.Router
-}
-
-func (cm *CORSMiddleware) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if origin := req.Header.Get("Origin"); origin != "" {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	}
-
-	if req.Method == "OPTIONS" {
-		// Stop here if its Preflighted OPTIONS request
-		return
-	}
-
-	cm.router.ServeHTTP(w, req)
-}
-
-func main() {
+func parseEnvironment() (string, string, string) {
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
 		redisURL = "redis://:@127.0.0.1:6379"
@@ -71,11 +40,21 @@ func main() {
 		log.Println("environment variable PORT is not set, defaults to " + port)
 	}
 
+	authenticationToken := os.Getenv("ACCESS_TOKEN")
+	if authenticationToken == "" {
+		log.Println("ACCESS_TOKEN environment variable is not set, defaults to empty token")
+	}
+
+	return redisURL, port, authenticationToken
+}
+
+func main() {
+	redisURL, port, authToken := parseEnvironment()
 	pool = newPool(redisURL)
 
 	srv := http.Server{
 		Addr:    ":" + port,
-		Handler: &CORSMiddleware{appHandler()},
+		Handler: appHandler(authToken),
 	}
 
 	log.Printf("sogimood-backend is running on port " + port + " bro' :)")
