4a3833865509425cdd1880710cefee0ccd34dde3
Marien Fressinaud <marien@sogilis.com>
2016-09-17 17:10:27 +0200

    frontend/chore/Extract AppContainer

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,21 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import moment from 'moment'
 import 'moment-range'
 
 import Store from './Store'
 import rootReducer from './reducer'
-import App from './App'
+import AppContainer from './AppContainer'
 
 import './style/index.css'
 import './style/font-awesome.min.css'
 
 moment.locale('fr')
 
-class AppContainer extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {}
-
-    this.store = new Store(rootReducer)
-    this.store.subscribe(this.setState.bind(this))
-  }
-
-  render() {
-    return (
-      <App
-        appState={ this.store.getState() }
-        dispatch={ this.store.dispatch.bind(this.store) }
-      />
-    )
-  }
-}
+const store = new Store(rootReducer)
 
 ReactDOM.render(
-  <AppContainer />,
+  <AppContainer store={ store } />,
   document.getElementById('root')
 )
